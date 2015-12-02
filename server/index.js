d1f4d8db143d816bbf3b310a0ea4a59d1f12d2fa
Ezequiel García <ezeezegg@gmail.com>
2015-12-02 15:20:07 -0600

    add middlewares for rest api

--- server/index.js
+++ server/index.js
@@ -3,3 +3,12 @@ var mongojs = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var _ = require('lodash');
+
+
+//create the application
+var app = express();
+
+//add middleware necessary for REST API's
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
