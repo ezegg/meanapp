9d5c3a1e6d21033eec9bddcfa2c296d2987479fe
Ezequiel García <ezeezegg@gmail.com>
2015-12-02 15:22:44 -0600

    connect with db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var mongojs = require('mongoose');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var _ = require('lodash');
@@ -12,3 +12,10 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
+
+//connect to MongoDB
+mongoose.connect('mongodb://localhost/meanapp');
+mongoose.connection.once('open', function  () {
+	console.log('Listening on port 3000....');
+	app.listen(3000);
+})
